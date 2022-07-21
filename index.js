b158a844be5e82ba99c2f54ccdcfa920d9a99559
amydesigns001 <105316350+amydesigns001@users.noreply.github.com>
2022-07-21 17:54:16 -0400

    7/21/22

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+document.querySelectorAll('.nav_decoration').forEach(link => {
+  if(link.href === window.location.href){
+    link.setAttribute('aria-current', 'page')
+  }
 })
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
