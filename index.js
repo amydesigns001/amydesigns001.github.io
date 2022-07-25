b81b9d7561bf65b4b8a94dfd642672734fbc60ed
amydesigns001 <105316350+amydesigns001@users.noreply.github.com>
2022-07-25 19:45:06 -0400

    Responsive Hamburger

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ document.querySelectorAll('.nav_decoration').forEach(link => {
     link.setAttribute('aria-current', 'page')
   }
 })
+
+let hamburger = document.querySelector('.hamburger_icon');
+let menu = document.querySelector('.navbar_flexbox');
+let bod = document.querySelector('.nav_container');
+
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('isactive');
+  menu.classList.toggle('active');
+
+});
