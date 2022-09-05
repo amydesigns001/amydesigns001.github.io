a70cd189cc3364925fdfb138151a2a512ebe75ff
amydesigns001 <105316350+amydesigns001@users.noreply.github.com>
2022-09-05 18:18:10 -0400

    ecomm

--- index.js
+++ index.js
@@ -12,4 +12,22 @@ hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('isactive');
   menu.classList.toggle('active');
 
-});
+})
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+
+})
